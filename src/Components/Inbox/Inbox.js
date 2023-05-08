81b7526c45121d2f2bdca9f1a29bdd55f5ae1e69
AnuraagChetia <anuraagchetia77@gmail.com>
2023-05-08 18:54:12 +0530

    created inbox

--- src/Components/Inbox/Inbox.js
+++ src/Components/Inbox/Inbox.js
@@ -2,21 +2,25 @@ import React from "react";
 import { ListGroupItem, ListGroup } from "react-bootstrap";
 const Inbox = (props) => {
   return (
-    <div className="p-2 border border-dark m-2">
+    <div className="  p-2 border border-dark m-2">
       <h1>INBOX</h1>
       <ListGroup as="ol" numbered>
-        <ListGroupItem
-          as="li"
-          variant="info"
-          action
-          className="d-flex justify-content-between align-items-start"
-        >
-          <div className="ms-2 me-auto">
-            <div className="fw-bold"> Sender</div>
-            Subject
-            <span>Message</span>
-          </div>
-        </ListGroupItem>
+        {props.inbox &&
+          props.inbox.map((mail) => (
+            <ListGroupItem
+              key={Math.random()}
+              as="li"
+              variant="info"
+              action
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-2 me-auto">
+                <div className="fw-bold"> {mail.sender}</div>
+                {mail.subject}
+                <span className="d-flex">{mail.message}</span>
+              </div>
+            </ListGroupItem>
+          ))}
       </ListGroup>
     </div>
   );
